9920e9e9d20efd08e1c1c335d08c5ec1c2ce129e
Ibiyemi Abiodun <laptou@users.noreply.github.com>
2018-10-30 05:04:45 -0400

    Fixed crash in Electron main process on Windows
    PR-URL: https://github.com/node-ffi-napi/get-uv-event-loop-napi-h/pull/4

--- include/get-uv-event-loop-napi.h
+++ include/get-uv-event-loop-napi.h
@@ -1,6 +1,12 @@
 #ifndef _GET_UV_EVENT_LOOP_NAPI_H
+#define _GET_UV_EVENT_LOOP_NAPI_H
 
+#include <assert.h>
+#include <node_api.h>
+
+#if !defined(NAPI_VERSION) || NAPI_VERSION < 2
 #include <get-symbol-from-current-process.h>
+#endif
 
 inline
 uv_loop_t* get_uv_event_loop(napi_env env) {
@@ -12,9 +18,14 @@ uv_loop_t* get_uv_event_loop(napi_env env) {
   assert(status == napi_ok);
   if (napi_version < 2)
     return uv_default_loop();
+
+#if defined(NAPI_VERSION) && NAPI_VERSION < 2
   napi_get_uv_event_loop__ = (get_uv_event_loop_fn)
       get_symbol_from_current_process("napi_get_uv_event_loop");
-  assert(napi_get_uv_event_loop__ != NULL);
+#else
+  napi_get_uv_event_loop__ = &napi_get_uv_event_loop;
+#endif
+
   uv_loop_t* ret;
   status = napi_get_uv_event_loop__(env, &ret);
   assert(status == napi_ok);
